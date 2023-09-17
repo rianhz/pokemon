5e4adc32da53409c5fb9c515d36a522f38e66372
rianhz <ryanhidayat.rh197@gmail.com>
2023-09-17 11:17:19 +0700

    initial commit

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,21 @@
 import ReduxProvider from "@/components/ReduxProvider/ReduxProvider";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import NextNProgress from "nextjs-progressbar";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ReduxProvider>
+      <NextNProgress
+        color='#29D'
+        startPosition={0.3}
+        stopDelayMs={200}
+        height={3}
+        showOnShallow={false}
+        options={{
+          showSpinner: false,
+        }}
+      />
       <Component {...pageProps} />
     </ReduxProvider>
   );
